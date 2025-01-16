b6ca3bd35b791539fbdea29683cdc185f81aca7d
IsmaelNaciriFernandez <ismaelnacirifdez@gmail.com>
2025-01-16 14:13:12 +0100

    Add project files.

---
+++ FirstCoreApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace FirstCoreApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HIIIII");
+        }
+    }
+}
